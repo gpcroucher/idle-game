0e1c69bb81218027d9b934db6fabc83fa34dfc90
George Croucher <gpcroucher@gmail.com>
2024-10-01 16:20:45 +0100

    added basic foundry functionality

--- src/rooms/Foundry.jsx
+++ src/rooms/Foundry.jsx
@@ -1,22 +1,38 @@
 import Activity from "../components/Activity";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./Foundry.css";
+import { decrementInventoryStack, getInventory } from "../utils/inventoryFuncs";
+import items from "../assets/items";
 
 export default function Foundry() {
-  const [sheets, setSheets] = useState(0);
-  const [hopperContents, setHopperContents] = useState([]);
+  const [sheets, setSheets] = useState(() => {
+    const memory = JSON.parse(localStorage.getItem("foundry-sheets"));
+    return memory ? memory : 0;
+  });
+  const [hopperContents, setHopperContents] = useState(() => {
+    const memory = JSON.parse(localStorage.getItem("foundry-hopperContents"));
+    return memory ? memory : 0;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("foundry-sheets", JSON.stringify(sheets));
+  }, [sheets]);
+  useEffect(() => {
+    localStorage.setItem(
+      "foundry-hopperContents",
+      JSON.stringify(hopperContents)
+    );
+  }, [hopperContents]);
 
   return (
     <div className="room room-foundry">
       <h2>Foundry</h2>
-      <Activity
-        activityTitle={"activity-foundry-add-to-hopper"}
-        onEnd={addToHopper}
-        activityName={"Add an item to the hopper"}
-      />
+      <p>Add an item to the hopper:</p>
+      {displayHopperables()}
       <Activity
         activityTitle={"activity-foundry-melt-steel"}
-        onEnd={handleMeltButton}
+        onBegin={checkHopper}
+        onEnd={doMelt}
         baseTime={5000}
         activityName="Melt some scrap!"
       />
@@ -24,19 +40,50 @@ export default function Foundry() {
     </div>
   );
 
-  function addToHopper() {
-    setHopperContents([
-      ...hopperContents,
-      {
-        id: 0,
-        name: "Useless Sprocket",
-        description: "A toothed wheel with several teeth missing or bent",
-      },
-    ]);
-    console.log(hopperContents);
+  function addToHopper(event) {
+    const thisItemStack = {
+      item: items[event.target.id.split("#")[1]],
+      count: 1,
+    };
+    console.log(thisItemStack);
+    setHopperContents(hopperContents + 1);
+    decrementInventoryStack(thisItemStack.item);
   }
 
-  function handleMeltButton() {
+  function checkHopper() {
+    if (hopperContents >= 4) {
+      return true;
+    }
+  }
+
+  function displayHopperables() {
+    const hopperables = getHopperablesFromInventory().map((itemstack) => (
+      <div key={itemstack.item.id}>
+        <p>
+          {itemstack.item.name} x{itemstack.count}
+        </p>
+        <button
+          onClick={addToHopper}
+          id={`hopperableButton#${itemstack.item.id}`}
+        >
+          Add to Hopper
+        </button>
+      </div>
+    ));
+    return hopperables.length > 0 ? (
+      hopperables
+    ) : (
+      <p>{"You don't have any meltable items."}</p>
+    );
+  }
+
+  function doMelt() {
     setSheets(sheets + 1);
+    setHopperContents(hopperContents - 4);
+  }
+
+  function getHopperablesFromInventory() {
+    const inventory = getInventory();
+    return inventory.filter((itemstack) => itemstack.item.meltable === "steel");
   }
 }
